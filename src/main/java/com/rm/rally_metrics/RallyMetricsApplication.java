68f5c2f40c66e6848e6ad18e55317f6db4874a6d
Vincent Pineda <vin.osu88@gmail.com>
2025-05-31 02:11:03 -0700

    backend stuff

--- src/main/java/com/rm/rally_metrics/RallyMetricsApplication.java
+++ src/main/java/com/rm/rally_metrics/RallyMetricsApplication.java
@@ -3,6 +3,7 @@ package com.rm.rally_metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RallyMetricsApplication {
 
